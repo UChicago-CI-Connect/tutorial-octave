956540818c352041c12e9eddf7bfcdf84ea1b257
Balamurugan Desinghu <bala@nsit-dhcp-250-206.uchicago.edu>
2014-10-07 15:34:54 -0500

    Octave on RCC

--- octave-wrapper.sh
+++ octave-wrapper.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
  
-EXPECTED_ARGS=1
- 
-if [ $# -ne $EXPECTED_ARGS ]; then
-  echo "Usage: octave-wrapper.sh file.octave"
-  exit 1
-else
   source /cvmfs/oasis.opensciencegrid.org/osg/palms/sw/octave/el5/x86_64/default/setup.sh
-  octave $1
-fi
+  module load octave 
+  octave ex1_matrix.octave 
