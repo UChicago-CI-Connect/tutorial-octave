0d5e4a3b36d04a8d5726e3daed94b9f6f36032de
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2014-11-06 16:23:21 -0600

    updated to use DEM

--- octave-wrapper.sh
+++ octave-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  
-  source /cvmfs/oasis.opensciencegrid.org/osg/palms/sw/octave/el5/x86_64/default/setup.sh
+  source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
   module load octave 
   octave ex1_matrix.octave 
