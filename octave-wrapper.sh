8ebbe730fc3b78970c7c33139947aa30d5cd7cff
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2014-11-06 16:28:17 -0600

    updated to add dependencies

--- octave-wrapper.sh
+++ octave-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
  
   source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
+  module load fftw/3.3.4 atlas lapack hdf5 qhull pcre SparseSuite glpk octave
   module load octave 
   octave ex1_matrix.octave 
