ae88c78aae7b35af03fc1edc2b75a3f5625ff034
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-04-10 16:28:03 -0500

    Don't need to source lmod file anymore

--- octave-wrapper.sh
+++ octave-wrapper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-  source /cvmfs/oasis.opensciencegrid.org/osg/palms/sw/octave/el5/x86_64/default/setup.sh
-  module load octave 
-  octave ex1_matrix.octave 
+module load octave 
+octave ex1_matrix.octave 
 
